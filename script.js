0c573d6ed69a31257b91456ce1bf70f851bc5b7f
cafe51 <cafecafe51@hotmail.com>
2022-02-21 18:18:00 -0400

    requisito 5 resolvido

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function corAserAdvinhada() {
-  const cor = document.querySelector('#rgb-color');
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  cor.innerText = `(${r}, ${g}, ${b})`;
-}
-
 function opcoesDeCores() {
   for (let i = 0; i < 6; i += 1) {
     const balls = document.createElement('div');
@@ -18,6 +10,60 @@ function opcoesDeCores() {
     sectionOpcoes.appendChild(balls);
   }
 }
-corAserAdvinhada();
+
+function corAserAdvinhada() {
+  const balls2 = document.querySelectorAll('.ball');
+  const cor = document.querySelector('#rgb-color');
+  const i = Math.floor(Math.random() * 6);
+  cor.innerText = `${balls2[i].style.backgroundColor}`;
+}
+
+// Math.floor(Math.random() * 6)
+
+// function(evt) {
+//   evt.target
+// }
+
+// function resposta() {
+//     const balls = querySelector('.ball');
+//     for (let i = 0; i < balls.length; i += 1) {
+//   }
+// }
+
+function responde(evt) {
+  // const balls = document.querySelectorAll('.ball');
+  const corCerta = document.querySelector('#rgb-color');
+  const mensagem = document.querySelector('#answer');
+  if (evt.target.style.backgroundColor === corCerta.innerText) {
+    mensagem.innerText = 'Acertou!';
+  } else mensagem.innerText = 'Errou! Tente novamente!';
+}
+
+function jogo() {
+  const balls = document.querySelectorAll('.ball');
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].addEventListener('click', responde);
+  }
+}
 
 opcoesDeCores();
+
+corAserAdvinhada();
+
+jogo();
+
+// function jogo() {
+//   const balls = document.querySelectorAll('.ball');
+//   const corCerta = document.querySelector('#rgb-color');
+//   const mensagem = document.querySelector('#answer');
+//   for (let i = 0; i < balls.length; i += 1) {
+//     balls[i].addEventListener('click', function() {
+//       for (let i = 0; i < balls.length; i += 1){
+//         if (`${balls[i].style.backgroundColor}` === `${corCerta.style.backgroundColor}`) {
+//           mensagem.innerText = 'Acertou!';
+//         } else mensagem.innerText = 'Errou!';
+//       }
+      
+//     });
+//   }
+// }
