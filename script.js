a0bb85721acefb5415a77160d6019f95c8553abd
cafe51 <cafecafe51@hotmail.com>
2022-02-21 16:28:14 -0400

    requisitos 3 e 4 resolvidos

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function corAserAdvinhada() {
   const cor = document.querySelector('#rgb-color');
-  cor.innerText = '(168, 34, 1)';
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  cor.innerText = `(${r}, ${g}, ${b})`;
 }
 
+function opcoesDeCores() {
+  for (let i = 0; i < 6; i += 1) {
+    const balls = document.createElement('div');
+    balls.className = 'ball';
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    balls.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    const sectionOpcoes = document.querySelector('#opcoes');
+    sectionOpcoes.appendChild(balls);
+  }
+}
 corAserAdvinhada();
+
+opcoesDeCores();
